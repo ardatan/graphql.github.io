61e9f8899caa3d51ea4df84ecab94520d5e8999c
dipeshwagle <dipeshwagle@icloud.com>
2020-08-16 16:41:01 +0545

    delete old files

---
+++ gatsby-config.js
@@ -0,0 +1,57 @@
+module.exports = {
+  siteMetadata: {
+    title: "A query language for your API",
+    description:
+      "GraphQL provides a complete description of the data in your API, gives clients the power to ask for exactly what they need and nothing more, makes it easier to evolve APIs over time, and enables powerful developer tools.",
+    siteUrl: "http://graphql.org/",
+  },
+
+  plugins: [
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "learn",
+        path: `${__dirname}/src/content`,
+        include: ["**/*.md"], // ignore files starting with a dot
+      },
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-prismjs",
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: null,
+              aliases: { js: "javascript" },
+              showLineNumbers: false,
+              noInlineHighlight: false,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Rubik`,
+            variants: [`300`],
+          },
+          {
+            family: `Roboto`,
+            variants: [`300`],
+          },
+          {
+            family: `Roboto Mono`,
+            variants: [`400`, `400i`, `600`],
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-less`,
+    `gatsby-plugin-react-helmet`,
+  ],
+}
