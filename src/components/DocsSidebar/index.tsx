3ca73316bcd0bd54dc17188b5ce558b33bd3bb8e
dipeshwagle <dipeshwagle@icloud.com>
2020-08-24 21:59:47 +0545

    fix content design

--- src/components/DocsSidebar/index.tsx
+++ src/components/DocsSidebar/index.tsx
@@ -1,20 +1,19 @@
 import React from "react"
 import { Link } from "gatsby"
-import {toSlug} from '../../utils/slug';
-
+import { toSlug } from "../../utils/slug"
 
 const SidebarForCategory = ({ category }: any) => {
-  const listItems = category.links.map( (page:any) => {
+  const listItems = category.links.map((page: any) => {
     return (
       <li key={page.frontmatter.permalink}>
         <Link
           style={{ marginLeft: page.indent ? 20 : 0 }}
-          activeClassName="active"
+          className="active"
           to={page.frontmatter.permalink}
         >
           {page.frontmatter.sidebarTitle || page.frontmatter.title}
         </Link>
-        {page.frontmatter.sublinks && ( // Sublinks to any page sub-parts
+        {page.frontmatter.sublinks && (
           <ul>
             {page.frontmatter.sublinks.split(",").map((sublink: any) => (
               <li key={sublink}>
